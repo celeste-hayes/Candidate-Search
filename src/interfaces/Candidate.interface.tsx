f4d2559510bbee009678f6dac5605978af0f1a0b
Celeste Hayes <celestehayes@Mac.attlocal.net>
2025-01-21 22:23:09 -0500

    Feat: candidate interface for API

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,11 @@
-// TODO: Create an interface for the Candidate objects returned by the API
+interface Candidate {
+    id: number;
+    login: string;
+    avatar_url: string;
+    location: string | null;
+    email: string | null;
+    company: string | null;
+    bio: string | null;
+}
+
+export default Candidate;
